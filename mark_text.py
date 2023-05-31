5924598d8346c839d7df3481f76687b22e843df7
Nicholas Gunther <nlgunther@gmail.com>
2023-05-31 18:32:19 -0400

    Update to mark_text.py to faciliate import by adding flags that by default 1) load as text the parsed pdf pages from a pickle file and 2) do not display them.

--- mark_text.py
+++ mark_text.py
@@ -1,4 +1,4 @@
-import numpy as np, re, PyPDF2 as ppdf2, pickle as pkl
+import numpy as np, re, PyPDF2 as ppdf2, pickle as pkl, os
 from tqdm import tqdm
 text = 'ablankcblankde'
 rex = re.compile('blank')
@@ -18,10 +18,23 @@ out = mark(matches[1],text,'_START_','_END_')
 out = mark(matches[1],text,end_marker='_END_')
 out = mark(matches[1],text,'_START_')
 
-reader = ppdf2.PdfReader("Downloads\connave-2016-c01-prospectus(1).pdf")
-# pages = [reader.pages[i].extract_text() for i in tqdm(range(reader.numPages))]
-# pkl.dump(pages,open('sample_cas.pkl','wb'))
-pages = pkl.load(open('sample_cas.pkl','rb'))
+# get files from Downloads folder
+from_downloads = lambda fn: os.path.join("..\..\..\Downloads",fn)
+sample_cas_fn = from_downloads('sample_cas.pkl')
+
+read_from_pdf = False
+
+if read_from_pdf:
+
+    reader = ppdf2.PdfReader(from_downloads("connave-2016-c01-prospectus(1).pdf"))
+    pages = [reader.pages[i].extract_text() for i in tqdm(range(reader.numPages))]
+
+    pkl.dump(pages,open(sample_cas_fn,'wb'))
+
+else:
+    
+    assert os.path.isfile(sample_cas_fn)
+    pages = pkl.load(open(sample_cas_fn,'rb'))
 # print(pages[25])
 isx = lambda x: x[1]
 rex = 'Allocation of Senior Reduction Amount and Subordinate Reduction Amount'
@@ -29,6 +42,10 @@ relpages_ = filter(isx,[(i,get_matches(page,rex)) for i,page in enumerate(pages)
 relpages = (list(relpages_))
 insp_match_ = lambda text,match,margin=100: text[max(0,match.start()-margin):min(len(text),match.end() + margin)]
 insp_match = lambda text,matches,margin=100: [insp_match_(text,match,margin) for match in matches]
-for i, matches in relpages:
-    print('\n%i\n-----------------\n' % i)
-    print(i,'\n#####\n'.join(insp_match(pages[i],matches)))
+
+display_replages = False
+
+if display_replages:
+    for i, matches in relpages:
+        print('\n%i\n-----------------\n' % i)
+        print(i,'\n#####\n'.join(insp_match(pages[i],matches)))
