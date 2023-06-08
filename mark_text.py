c8e9aac92ed9bfd07fa826ddd7fe928ea33acf6f
Nicholas Gunther <nlgunther@gmail.com>
2023-06-08 06:15:30 -0400

    mark_test.py updated with a class.

--- mark_text.py
+++ mark_text.py
@@ -8,7 +8,6 @@ get_matches = lambda text,rex: list(re.finditer(rex,text))
 def mark_(match, text, marker):
     start = match.start()
     return text[:start]+marker+text[start:]
-matches = get_matches(text,rex)
 def mark(match,text_,start_marker=None,end_marker=None):
     text = text_[:]
     for marker in (start_marker,end_marker):
@@ -49,3 +48,92 @@ if display_replages:
     for i, matches in relpages:
         print('\n%i\n-----------------\n' % i)
         print(i,'\n#####\n'.join(insp_match(pages[i],matches)))
+
+class TextMarker(object): 
+    
+    @staticmethod
+    def forfilter(f): return lambda x: f(x)
+    
+    second = staticmethod(lambda x: x[1])
+
+    @staticmethod
+    def get_match(text,rex): return list(re.finditer(rex,text))
+    
+    @staticmethod
+    def mark_(match, text, marker):
+        start = match.start()
+        return text[:start]+marker+text[start:]
+    
+    @staticmethod
+    def mark(match,text_,start_marker=None,end_marker=None):
+        text = text_[:]
+        for marker in (start_marker,end_marker):
+            if marker: text = mark_(match,text,marker)
+        return text  
+
+    @staticmethod
+    def insp_match_(text,match,margin=100,upper=True):
+        matchtext = match.group()
+        if upper: matchtext = matchtext.upper()
+        return text[max(0,match.start()-margin):match.start()]+\
+                    match.group().upper() +\
+                    text[match.end():min(len(text),match.end() + margin)]
+    
+    @classmethod
+    def get_matches_(kls,texts,rex): return list(filter(kls.second,[(i,kls.get_match(text,rex))
+                                                                    for i,text in enumerate(texts)]))
+   
+    @classmethod
+    def insp_match(kls,text,matches,margin=100): return [kls.insp_match_(text,match,margin) for match in matches]
+
+    
+    def __init__(self,start_marker,end_marker,pages):
+        self.start_marker = start_marker
+        self.end_marker = end_marker
+        self.pages = pages
+        
+    def get_matches(self,rex):
+        self.matches = self.get_matches_(self.pages,rex)
+        return len(self.matches)
+    
+    
+    def mark(self,rex,matches_num,
+             match_num = 0,
+             start=None,
+             end=None,
+             text = None):
+        print(matches_num)
+        pagenum,matches = self.matches[matches_num]
+        text = text if text else self.pages[pagenum][:]
+        match = matches[match_num]
+#         print('match',match)
+        markers = filter(self.__class__.second,zip((self.start_marker,self.end_marker),(start,end)))
+        for marker,_ in markers: 
+            text = self.mark_(match,text,marker)
+        return text
+    
+    def excerpt(self,margin=100):
+        return [(i,matches_[0], self.insp_match(self.pages[matches_[0]],matches_[1],margin=margin))
+                for i, matches_ in enumerate(self.matches)]
+        
+    def display(self):
+            for i,j,excerpt in self.excerpt():
+                print('\nmatched_page_number %i, page number %i, number of matches on page %i\n-----------------\n' % (i,j,len(excerpt)))
+                print('\n###\n'.join(excerpt))
+
+assert 2 == TextMarker.second((1,2))
+
+rexar = 'Allocation of Senior Reduction Amount and Subordinate Reduction Amount;Allocation of Calculated Tranche Write.*down Amounts;Allocation of Calculated Tranche Write.*up Amounts'.split(';')
+rexs = '|'.join(rexar)
+
+tm = TextMarker(*['_%s:ALLOCATION' %s for s in 'START END'.split()],pages = pages)
+
+tm.get_matches(rexs)
+tm.display()
+
+# print('\n\n'.join(map(str,tm.excerpt(200))))
+if False: print('\n\n'.join(map(str,
+                      filter(lambda x:  re.search('(?i)On each Payment Date',
+                                ' '.join(x[2]).replace('\n',' ')),
+                                 tm.excerpt(200)))))
+
