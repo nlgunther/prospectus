bab4cf88e504b94cdceee276f566e29e89639b9d
Nicholas Gunther <nlgunther@gmail.com>
2023-06-15 14:01:32 -0400

    modified mark_text.py found but not yet committed, so I committed it.

--- mark_text.py
+++ mark_text.py
@@ -2,20 +2,6 @@ import numpy as np, re, PyPDF2 as ppdf2, pickle as pkl, os
 from tqdm import tqdm
 text = 'ablankcblankde'
 rex = re.compile('blank')
-get_matches = lambda text,rex: list(re.finditer(rex,text))
-
-
-def mark_(match, text, marker):
-    start = match.start()
-    return text[:start]+marker+text[start:]
-def mark(match,text_,start_marker=None,end_marker=None):
-    text = text_[:]
-    for marker in (start_marker,end_marker):
-        if marker: text = mark_(match,text,marker)
-    return text
-out = mark(matches[1],text,'_START_','_END_')
-out = mark(matches[1],text,end_marker='_END_')
-out = mark(matches[1],text,'_START_')
 
 # get files from Downloads folder
 from_downloads = lambda fn: os.path.join("..\..\..\Downloads",fn)
@@ -37,17 +23,6 @@ else:
 # print(pages[25])
 isx = lambda x: x[1]
 rex = 'Allocation of Senior Reduction Amount and Subordinate Reduction Amount'
-relpages_ = filter(isx,[(i,get_matches(page,rex)) for i,page in enumerate(pages)])
-relpages = (list(relpages_))
-insp_match_ = lambda text,match,margin=100: text[max(0,match.start()-margin):min(len(text),match.end() + margin)]
-insp_match = lambda text,matches,margin=100: [insp_match_(text,match,margin) for match in matches]
-
-display_replages = False
-
-if display_replages:
-    for i, matches in relpages:
-        print('\n%i\n-----------------\n' % i)
-        print(i,'\n#####\n'.join(insp_match(pages[i],matches)))
 
 class TextMarker(object): 
     
@@ -111,7 +86,7 @@ class TextMarker(object):
         for marker,_ in markers: 
             text = self.mark_(match,text,marker)
         return text
-    
+        
     def excerpt(self,margin=100):
         return [(i,matches_[0], self.insp_match(self.pages[matches_[0]],matches_[1],margin=margin))
                 for i, matches_ in enumerate(self.matches)]
@@ -121,9 +96,24 @@ class TextMarker(object):
                 print('\nmatched_page_number %i, page number %i, number of matches on page %i\n-----------------\n' % (i,j,len(excerpt)))
                 print('\n###\n'.join(excerpt))
 
+relpages_ = filter(isx,[(i,TextMarker.get_matches_(page,rex)) for i,page in enumerate(pages)])
+tm = TextMarker('START_','END_',pages)
+
+relpages = (list(relpages_))
+insp_match_ = lambda text,match,margin=100: text[max(0,match.start()-margin):min(len(text),match.end() + margin)]
+insp_match = lambda text,matches,margin=100: [insp_match_(text,match,margin) for match in matches]
+
+display_replages = False
+
+if display_replages:
+    for i, matches in relpages:
+        print('\n%i\n-----------------\n' % i)
+        print(i,'\n#####\n'.join(insp_match(pages[i],matches)))
+
+
 assert 2 == TextMarker.second((1,2))
 
-rexar = 'Allocation of Senior Reduction Amount and Subordinate Reduction Amount;Allocation of Calculated Tranche Write.*down Amounts;Allocation of Calculated Tranche Write.*up Amounts'.split(';')
+rexar = 'Allocation of Senior Reduction Amount and Subordinate Reduction Amount;Allocation of Tranche Write.*down Amounts;Allocation of Tranche Write.*up Amounts'.split(';')
 rexs = '|'.join(rexar)
 
 tm = TextMarker(*['_%s:ALLOCATION' %s for s in 'START END'.split()],pages = pages)
