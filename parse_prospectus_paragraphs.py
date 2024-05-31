bf5d7dc7d9e51962cc6fc838a45284e02c2eb7b8
Nicholas Gunther <nlgunther@gmail.com>
2024-05-30 18:17:02 -0700

    Made proc_prosp() function to determine the paragraphs of a prospectus based on line spacing distances from pdfplumber.  Added and corrected imports.

--- parse_prospectus_paragraphs.py
+++ parse_prospectus_paragraphs.py
@@ -1,21 +1,33 @@
-import pdfplumber as pbr
-import StringIO as sio
-pbdf = pbr.open(path)
+import pdfplumber as pbr, numpy as np, os
+from io import StringIO as sio
+from sklearn.cluster import MeanShift
+from tqdm.notebook import tqdm
 
-get_dists = lambda lines: [line2['top'] - line1['bottom'] for line1,line2 in zip(lines[:-1],lines[1:])]
+dr = r'Downloads\fnma_cas'
+fn  = r'connave-2021-r01-offering-memorandum.pdf'
 
-pagear = []
-pages = pbdf.pages
-for page in pages:
-    lines = page.extract_text_lines()
-    dists = np.array(get_dists(lines))
-    if not dists.size: continue
-    ms = MeanShift().fit(dists.reshape(-1, 1))
-    clusters = ms.predict(dists.reshape(-1, 1))
-    nobreak = clusters[np.argmin(dists)]
-    siox = sio()
-    for line,label in zip(lines,clusters):
-        siox.write(line['text'])
-        siox.write('\n') if label != nobreak else siox.write(' ')
-    pagear.append(siox.getvalue())
-    siox.close()
+samplepath = os.path.join(dr,fn)
+path = samplepath
+
+
+def proc_prosp(path):
+    pbdf = pbr.open(path)
+
+    get_dists = lambda lines: [line2['top'] - line1['bottom'] for line1,line2 in zip(lines[:-1],lines[1:])]
+
+    pagear = []
+    pages = pbdf.pages
+    for page in tqdm(pages):
+        lines = page.extract_text_lines()
+        dists = np.array(get_dists(lines))
+        if not dists.size: continue
+        ms = MeanShift().fit(dists.reshape(-1, 1))
+        clusters = ms.predict(dists.reshape(-1, 1))
+        nobreak = clusters[np.argmin(dists)]
+        siox = sio()
+        for line,label in zip(lines,clusters):
+            siox.write(line['text'])
+            siox.write('\n') if label != nobreak else siox.write(' ')
+        pagear.append(siox.getvalue())
+        siox.close()
+    return pagear
