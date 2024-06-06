195b781b3979b0f50027d3082a2da5d95f044596
Nicholas Gunther <nlgunther@gmail.com>
2024-06-06 13:46:55 -0700

    Uncommented and edited MakeParagraphs.  Unsatisfactory experience with notebook calls.

--- parse_prospectus_paragraphs.py
+++ parse_prospectus_paragraphs.py
@@ -17,7 +17,10 @@ class LineTests(object):
     def get_individual_test_results(self,lineobj):
         return [test(lineobj) for test in self.tests]
     
-    def test(self, lineobj:dict, func:Callable = lambda arg: None) -> list[bool]:
+    def test(self, lineobj:dict, func:Callable = all) -> list[bool]:
+        '''
+        Use func = lambda arg: None to get list of individual test results
+        '''
         out = self.get_individual_test_results(lineobj)
         outf = func(out)
         return outf if outf != None else out
@@ -27,8 +30,8 @@ class LinesTests(LineTests):
     One or more tests, one or more line objects
     '''
 
-    def get_individual_test_results(self,lineobjs):
-        return [b for test in self.tests for b in test(lineobjs)]
+    # def get_individual_test_results(self,lineobjs):
+    #     return [b for test in self.tests for b in test(lineobjs)]
 
 dr = r'Downloads\fnma_cas'
 fn  = r'connave-2021-r01-offering-memorandum.pdf'
@@ -37,9 +40,9 @@ path_ = os.path.join(dr,fn)
 path = path_ if os.path.isfile(path_) else os.path.join(r'..\..\..',path_)
 print(path)
 
-removepagenum = lambda lines: lines[:-1 if pagenumtest.test(lines[-1]) else lines]
+pagenumtest = LineTests(lambda line: re.search('^\d+|[xiv]+$',line['text'].strip()))
 
-pagenumtest = LineTests(lambda line: re.search('\d+',line['text'].strip()))
+removepagenum = lambda lines: lines[:-1] if pagenumtest.test(lines[-1]) else lines
 
 lastlinepunctest = LineTests(lambda line: line['text'].strip()[-1] in '. ? ! : ,'.split())
 lastlineendtest = LineTests(lambda line: line['x1'] < 500)
@@ -61,40 +64,41 @@ linetests = LinesTests(
 
 
 
-# class MakeParagraphs(object):
+class MakeParagraphs(object):
 
-#     @staticmethod
-#     def get_dists(lines:list[Any], # typically output from pdfplumber extract_text_lines()
-#                   features = 'top bottom'.split(),
-#                   ) -> list[float]: return [line2[features[0]] - line1[features[1]] for line1,line2 in zip(lines[:-1],lines[1:])]
+    @staticmethod
+    def get_dists(lines:list[Any], # typically output from pdfplumber extract_text_lines()
+                  features = 'top bottom'.split(),
+                  ) -> list[float]: return [line2[features[0]] - line1[features[1]] for line1,line2 in zip(lines[:-1],lines[1:])]
 
-#     @staticmethod
-#     def test_para_end(line: Any):pass
+    @staticmethod
+    def test_para_end(line: Any):pass
 
-#     def __init__(pbrobj: Any,test:bool =True):
-#         pass
-        
+    def __init__(self,pbrobj: Any,test:bool =True):
+        self.pbrobj = pbrobj
 
-
-#     def proc_prosp(self,path):
-#         pbdf = pbr.open(path)
-#         pagear = []
-#         pages = pbdf.pages
-#         for page in tqdm(pages):
-#             lines = page.extract_text_lines()
-#             dists = np.array(get_dists(lines))
-#             if not dists.size: continue
-#             ms = MeanShift().fit(dists.reshape(-1, 1))
-#             clusters = ms.predict(dists.reshape(-1, 1))
-#             nobreak = clusters[np.argmin(dists)]
-#             siox = sio()
-#             for line,label in zip(lines[:-1],clusters): # distances to next line stop at lines[-2]
-#                 siox.write(line['text'])
-#                 siox.write('\n') if label != nobreak else siox.write(' ')
-#             siox.write(lines[-1]['text'])
-#             pagear.append(siox.getvalue())
-#             siox.close()
-#         return pagear
+    def proc_prosp(self):
+        pages = self.pbrobj.pages
+        last_lines,first_lines,pagelinear = [],[],[]
+        for page in tqdm(pages):
+            lines = page.extract_text_lines()
+            if lines:
+                lines = removepagenum(lines)
+                last_lines.append(lines[-1])
+                first_lines.append(lines[0])
+                dists = np.array(self.get_dists(lines))
+                if not dists.size: continue
+                ms = MeanShift().fit(dists.reshape(-1, 1))
+                clusters = ms.predict(dists.reshape(-1, 1))
+                nobreak = clusters[np.argmin(dists)]
+                siox = sio()
+                for line,label in zip(lines[:-1],clusters): # distances to next line stop at lines[-2]
+                    siox.write(line['text'])
+                    siox.write('\n') if label != nobreak else siox.write(' ')
+                siox.write(lines[-1]['text'])
+                pagelinear.append(siox.getvalue())
+                siox.close()
+        return pagelinear,first_lines,last_lines
 
 class TestBase(unittest.TestCase):
     pass
